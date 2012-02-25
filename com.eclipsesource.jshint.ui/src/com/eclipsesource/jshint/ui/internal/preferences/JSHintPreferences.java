4e0a876725444a4d6eb0b78262ff0c01a13a8d87
Ralf Sternberg <rsternberg@eclipsesource.com>
2012-02-25 15:10:08 +0100

    Made JSHintPreferences thread safe

--- com.eclipsesource.jshint.ui/src/com/eclipsesource/jshint/ui/internal/preferences/JSHintPreferences.java
+++ com.eclipsesource.jshint.ui/src/com/eclipsesource/jshint/ui/internal/preferences/JSHintPreferences.java
@@ -10,6 +10,10 @@
  ******************************************************************************/
 package com.eclipsesource.jshint.ui.internal.preferences;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -20,6 +24,16 @@ import org.osgi.service.prefs.BackingStoreException;
 import com.eclipsesource.jshint.ui.internal.Activator;
 
 
+/**
+ * Instances of this class provide a copy of the global JSHint preferences. The preference values
+ * are read from the preference store on creation. Modified values will be written back to the store
+ * only when <code>save()</code> is called.
+ * <p>
+ * Instances of this class can be accessed concurrently from multiple threads. However, multiple
+ * instances should not be used to concurrently write values to the backing store, otherwise updates
+ * may be lost.
+ * </p>
+ */
 public class JSHintPreferences {
 
   private static final String KEY_USE_CUSTOM_LIB = "useCustomJshint";
@@ -27,12 +41,17 @@ public class JSHintPreferences {
   private static final boolean DEF_USE_CUSTOM_LIB = false;
   private static final String DEF_CUSTOM_LIB_PATH = "";
 
+  private final Lock readLock;
+  private final Lock writeLock;
   private final IEclipsePreferences node;
-  private boolean dirty;
   private boolean useCustomLib;
   private String customLibPath;
+  private boolean dirty;
 
   public JSHintPreferences() {
+    ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    readLock = readWriteLock.readLock();
+    writeLock = readWriteLock.writeLock();
     node = InstanceScope.INSTANCE.getNode( Activator.PLUGIN_ID );
     useCustomLib = node.getBoolean( KEY_USE_CUSTOM_LIB, DEF_USE_CUSTOM_LIB );
     customLibPath = node.get( KEY_CUSTOM_LIB_PATH, DEF_CUSTOM_LIB_PATH );
@@ -45,58 +64,101 @@ public class JSHintPreferences {
   }
 
   public boolean getUseCustomLib() {
-    return useCustomLib;
+    try {
+      readLock.lock();
+      return useCustomLib;
+    } finally {
+      readLock.unlock();
+    }
   }
 
   public void setUseCustomLib( boolean useCustomLib ) {
-    if( useCustomLib != this.useCustomLib ) {
-      this.useCustomLib = useCustomLib;
-      dirty = true;
+    try {
+      writeLock.lock();
+      if( useCustomLib != this.useCustomLib ) {
+        this.useCustomLib = useCustomLib;
+        dirty = true;
+      }
+    } finally {
+      writeLock.unlock();
     }
   }
 
   public String getCustomLibPath() {
-    return customLibPath;
+    try {
+      readLock.lock();
+      return customLibPath;
+    } finally {
+      readLock.unlock();
+    }
   }
 
   public void setCustomLibPath( String customLibPath ) {
-    if( !customLibPath.equals( this.customLibPath ) ) {
-      this.customLibPath = customLibPath;
-      dirty = true;
+    try {
+      writeLock.lock();
+      if( !customLibPath.equals( this.customLibPath ) ) {
+        this.customLibPath = customLibPath;
+        dirty = true;
+      }
+    } finally {
+      writeLock.unlock();
     }
   }
 
   public boolean hasChanged() {
-    return dirty;
+    try {
+      readLock.lock();
+      return dirty;
+    } finally {
+      readLock.unlock();
+    }
   }
 
   public void save() throws CoreException {
     putUseCustomLib();
     putCustomLibPath();
+    flushNode();
+    try {
+      writeLock.lock();
+      dirty = false;
+    } finally {
+      writeLock.unlock();
+    }
+  }
+
+  private void putUseCustomLib() {
+    try {
+      readLock.lock();
+      if( useCustomLib == DEF_USE_CUSTOM_LIB ) {
+        node.remove( KEY_USE_CUSTOM_LIB );
+      } else {
+        node.putBoolean( KEY_USE_CUSTOM_LIB, useCustomLib );
+      }
+    } finally {
+      readLock.unlock();
+    }
+  }
+
+  private void putCustomLibPath() {
+    try {
+      readLock.lock();
+      if( customLibPath.equals( DEF_CUSTOM_LIB_PATH ) ) {
+        node.remove( KEY_CUSTOM_LIB_PATH );
+      } else {
+        node.put( KEY_CUSTOM_LIB_PATH, customLibPath );
+      }
+    } finally {
+      readLock.unlock();
+    }
+  }
+
+  private void flushNode() throws CoreException {
     try {
       node.flush();
-      dirty = false;
     } catch( BackingStoreException exception ) {
       String message = "Failed to store preferences";
       Status status = new Status( IStatus.ERROR, Activator.PLUGIN_ID, message, exception );
       throw new CoreException( status );
     }
   }
-
-  private void putUseCustomLib() {
-    if( useCustomLib == DEF_USE_CUSTOM_LIB ) {
-      node.remove( KEY_USE_CUSTOM_LIB );
-    } else {
-      node.putBoolean( KEY_USE_CUSTOM_LIB, useCustomLib );
-    }
-  }
-
-  private void putCustomLibPath() {
-    if( customLibPath.equals( DEF_CUSTOM_LIB_PATH ) ) {
-      node.remove( KEY_CUSTOM_LIB_PATH );
-    } else {
-      node.put( KEY_CUSTOM_LIB_PATH, customLibPath );
-    }
-  }
-
 }
